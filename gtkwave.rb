d0df5b221495ac9e59c755255233619ec2157bdc
Youssef Ashraf <92830538+yousseeefashrraf@users.noreply.github.com>
2024-11-29 15:54:04 +0200

    Update gtkwave.rb

--- gtkwave.rb
+++ gtkwave.rb
@@ -5,20 +5,19 @@ class Gtkwave < Formula
   head "https://github.com/randomplum/gtkwave.git", branch: "master"
   revision 3
 
-  depends_on "desktop-file-utils" => :build # for update-desktop-database
-  depends_on "shared-mime-info" => :build
-  depends_on "gobject-introspection" => :build
-  depends_on "gtk-mac-integration" => :build
-  depends_on "meson" => :build
-  depends_on "ninja" => :build
-  depends_on "pkg-config" => :build
-  depends_on "gtk+3"
+depends_on "desktop-file-utils" => :build # for update-desktop-database
+depends_on "shared-mime-info" => :build
+depends_on "gobject-introspection" => :build
+depends_on "meson" => :build
+depends_on "ninja" => :build
+depends_on "pkg-config" => :build
+depends_on "gtk+3"
 
-  def install
-    ENV["DESTDIR"] = "/"
-    system "meson", "setup", "build", "-Dintrospection=false", *std_meson_args
-    system "meson", "compile", "-C", "build", "--verbose"
-    system "meson", "install", "-C", "build"
-  end
+def install
+ENV["DESTDIR"] = "/"
+system "meson", "setup", "build", *std_meson_args
+system "meson", "compile", "-C", "build", "--verbose"
+system "meson", "install", "-C", "build"
+end
 
 end
